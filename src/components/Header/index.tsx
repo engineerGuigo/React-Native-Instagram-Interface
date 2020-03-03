6cd117b9e8cc7c0d46ac0d747a06015c35a2813e
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-03-03 16:00:10 -0300

    logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Logo } from './styles';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
 
     return (
         <Container>
             <Icon name="camera" size={26} />
+            <Logo source={logo} />
             <Icon name="paper-plane" size={26} />
         </Container>
     );
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -4,3 +4,9 @@ export const Container = styled.SafeAreaView`
     flex: 1;
     background-color: blue;
 `;
+
+export const Logo = styled.Image.attrs({
+    resizeMode: 'contain'
+})`
+    height: 40px;
+`;
