7832557fa988bfb49fb8dbd325b12fc8f5086f67
Rodrigo Durante <rodrigodurante@Guilhermes-MacBook-Pro.local>
2020-03-03 01:43:09 -0300

    add styled-components

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './src/components/Header';
+
+const App = () => {
+
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+};
+
+export default App;
