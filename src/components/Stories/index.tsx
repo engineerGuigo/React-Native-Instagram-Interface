fff02b2ec792538a843a9b970f8758f89e961deb
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-03-03 18:28:01 -0300

    Stories Label Styling

--- src/components/Stories/index.tsx
+++ src/components/Stories/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Container, ContainerHeader, GroupLabel, Label, } from './styles';
 
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 const Stories = () => {
 
     return (
@@ -9,6 +11,7 @@ const Stories = () => {
             <ContainerHeader>
                 <Label>Stories</Label>
                 <GroupLabel>
+                    <Icon name='caret-right' size={20} />
                     <Label>Watch all</Label>
                 </GroupLabel>
             </ContainerHeader>
--- src/components/Stories/styles.tsx
+++ src/components/Stories/styles.tsx
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.View`
-    flex: 1;
     background-color: red;
+    padding: 10px;
 `;
 
-export const ContainerHeader = styled.View``;
-export const GroupLabel = styled.Text``;
-export const Label = styled.Text``;
+export const ContainerHeader = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+
+`;
+export const GroupLabel = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+export const Label = styled.Text`
+    font-size: 14px;
+    padding: 5px 10px;
+`;
