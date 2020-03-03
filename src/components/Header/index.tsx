1daca96e4751ef1ba566834de1117f547cadddce
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-03-03 15:07:25 -0300

    add camera and paper-plane

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 const Header = () => {
 
     return (
-        <Container></Container>
+        <Container>
+            <Icon name="camera" size={26} />
+            <Icon name="paper-plane" size={26} />
+        </Container>
     );
 };
 
